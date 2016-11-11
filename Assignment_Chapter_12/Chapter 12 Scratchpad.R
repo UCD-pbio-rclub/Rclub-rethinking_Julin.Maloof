8a0f7d0be13f9403ea4ac953de3af067791b3fd0
Julin Maloof <jnmaloof@ucdavis.edu>
2016-11-11 09:32:46 -0800

    update chapter 12 scratchpad

--- Assignment_Chapter_12/Chapter 12 Scratchpad.R
+++ Assignment_Chapter_12/Chapter 12 Scratchpad.R
@@ -72,3 +72,46 @@ sim_tanks <- rnorm( 8000 , post$a , post$sigma )
 # transform to probability and visualize
               
 dens( logistic(sim_tanks) , xlab="probability survive" )
+
+#simulations, section 12.2
+
+a <- 1.4
+sigma <- 1.5
+nponds <- 60
+ni <- as.integer( rep( c(5,10,25,35) , each=15 ) )
+
+a_pond <- rnorm( nponds , mean=a , sd=sigma )
+
+dsim <- data.frame( pond=1:nponds , ni=ni , true_a=a_pond )
+
+class(1:3)
+class(c(1,2,3))
+
+dsim$si <- rbinom( nponds , prob=logistic(dsim$true_a) , size=dsim$ni )
+
+dsim$p_nopool <- dsim$si / dsim$ni
+
+m12.3 <- map2stan(
+  alist(
+    si ~ dbinom( ni , p ),
+    logit(p) <- a_pond[pond],
+    a_pond[pond] ~ dnorm( a , sigma ),
+    a ~ dnorm(0,1),
+    sigma ~ dcauchy(0,1)
+  ),
+  data=dsim , iter=1e4 , warmup=1000 )
+
+precis(m12.3,depth=2)
+
+estimated.a_pond <- as.numeric(coef(m12.3)[1:60])
+
+dsim$p_partpool <- logistic(estimated.a_pond)
+
+dsim$p_true <- logistic( dsim$true_a )
+
+nopool_error <- abs( dsim$p_nopool - dsim$p_true )
+partpool_error <- abs( dsim$p_partpool - dsim$p_true )
+
+plot( 1:60 , nopool_error , xlab="pond" , ylab="absolute error" ,
+      col=rangi2 , pch=16 )
+points( 1:60 , partpool_error )
